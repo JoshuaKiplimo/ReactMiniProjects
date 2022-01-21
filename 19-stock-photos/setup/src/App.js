29365ce0170798ea04d18e70dfb79d8cabb8f6a6
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-01-20 22:04:42 -0500

    better way of infinite scrolling images

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -8,10 +8,11 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const searchRef = useRef();
+  const mounted = useRef(false);
   const [query, setQuery] = useState("");
-
+  const [newImages, setNewImages] = useState(false);
   const fetchData = async () => {
     try {
       setLoading(true);
@@ -34,6 +35,7 @@ function App() {
           return [...oldPhotos, ...data];
         }
       });
+      setNewImages(false); //set switch back to off after getting data
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -45,24 +47,43 @@ function App() {
     fetchData();
   }, [page]);
   useEffect(() => {
-    const event = window.addEventListener("scroll", () => {
-      let innerHeight = parseInt(window.innerHeight);
-      let scrollHeight = parseInt(window.scrollY);
-      let bodyHeight = parseInt(document.body.scrollHeight);
-      if (!loading && innerHeight + scrollHeight >= bodyHeight - 5) {
-        setPage((oldPage) => {
-          return oldPage + 1;
-        });
-      }
-    });
-    return () => window.removeEventListener("scroll", event);
-  }, []);
+    console.log("mounted.current", mounted.current);
+    if (!mounted.current) {
+      //this means that if we are in the initial render(which the mounted ref sets as false), we
+      //set it back to true to know whether the component was rerendered.
+      console.log(mounted.current);
+      mounted.current = true;
+      return;
+    }
+    //he two if statements below checks for both and when wea re at the point of
+    if (!newImages) return;
+    if (loading) return;
+    setPage((oldPage) => oldPage + 1);
+    console.log("initial render");
+  }, [newImages]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //fetchData();
+    if (!query) return;
+    if (page === 1) {
+      fetchData();
+      return;
+    }
     setPage(1);
   };
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", event);
+
+    return () => {
+      window.removeEventListener("scroll", event);
+    };
+  }, []);
 
   return (
     <main>

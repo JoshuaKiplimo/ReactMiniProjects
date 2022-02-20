5bbcfd07b43c5c95e2656c788d114047133e9905
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-02-19 23:12:08 -0500

    ADD

--- Github/src/context/context.js
+++ Github/src/context/context.js
@@ -14,11 +14,34 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [request, setRequest] = useState(0);
+  const [loading, setIsLoading] = useState(false);
   const fetchData = async (url) => {
     const data = await fetch(url);
     const languages = await data.json();
     return languages;
   };
+  //check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequest(remaining);
+        console.log(remaining);
+        if (remaining === 0) {
+          //throw error
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    checkRequests();
+    console.log("loaded");
+  }, []);
   return (
     <GithubContext.Provider value={{ repos, githubUser, followers, fetchData }}>
       {children}
